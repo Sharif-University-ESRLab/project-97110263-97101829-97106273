0594c1e1b02080ac229b52647c250dcdfb26c409
M Sadegh <sadeghclan93@gmail.com>
2022-08-02 13:18:35 +0430

    arduino code file is added.

---
+++ Code/coffee.ino
@@ -0,0 +1,19 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+
+void setup() {
+  // TODO: connect to wifi
+  // TODO: connect to server
+  Serial.begin(9600);
+  
+  Serial.println("Setup done");
+}
+
+void loop() {
+  // TODO: client must get updates from server
+
+  // TODO: codes about coffee maker parameters and actions must be done
+}
