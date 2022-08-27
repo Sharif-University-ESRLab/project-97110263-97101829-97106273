f5b7293695d75af95f4eebef38c2bfa0561116ac
M Sadegh <sadeghclan93@gmail.com>
2022-08-27 14:20:03 +0430

    add TODOs for iOS application/WiFi

--- Code/simpleCoffeeMaker/simpleCoffeeMaker.ino
+++ Code/simpleCoffeeMaker/simpleCoffeeMaker.ino
@@ -3,6 +3,9 @@
 
 Servo myservo;  // create servo object to control a servo
 
+// TODO: change pins for nodeMCU: consider that "servoDataPin" is PWM pulse
+// optional TODO: we don't need startPin: we read from the application
+
 int closeDegree = 170;          // variable to store the servo position for closing
 int openDegree = 90;            // variable to store the servo position for opening
 int armaturePin = 13;           // variable to set the pin of HIGH and LOW voltage for turn on and off armature
@@ -13,9 +16,14 @@ int startPin = 10;              // variable to read the pin for starting coffee 
 
 int coffeeLevel = 1;            // 1 to 3
 int waterLevel = 1;             // 1 to 2
-int coffeeOpenTime = 5000;      // time (ms) that coffee tank is open
-int waterOpenTime = 18000;       // time (ms) that water tank is open
-int coffeeTurnOnTime = 20000;  // time (ms) that coffee maker must be on and boiling
+int coffeeLevelUpUnit = 2000;   // for each level up
+int waterLevelUpUnit = 10000;   // for each level up
+
+int coffeeInitTime = 5000;      // first level time for coffee
+int waterInitTime = 18000;      // first level time for water
+int coffeeOpenTime;             // time (ms) that coffee tank is open
+int waterOpenTime;              // time (ms) that water tank is open
+int coffeeTurnOnTime = 40000;   // time (ms) that coffee maker must be on and boiling
 
 
 void setCoffeePower(bool isOn) {
@@ -90,17 +98,27 @@ void setup() {
 void loop() {
     int shouldStart = digitalRead(startPin);   // read the input pin
     
-    coffeeOpenTime = 2000 * coffeeLevel;
-    waterOpenTime = 18000 * waterLevel;
-
-    Serial.println(shouldStart);
+    // TODO: read shouldStart from application
+    // TODO: read coffeeLevel and waterLevel from application
     
+    coffeeOpenTime = coffeeInitTime + (coffeeLevel - 1) * coffeeLevelUpUnit;
+    waterOpenTime = waterInitTime + (waterLevel - 1) * waterLevelUpUnit;
+
+    Serial.println("shouldStart:");
+    Serial.println(shouldStart);
+    Serial.println("coffeeOpenTime:");
+    Serial.println(coffeeOpenTime);
+    Serial.println("waterOpenTime:");
+    Serial.println(waterOpenTime);
+
+    // TODO: if you read from application shouldStart must be active HIGH
     if (shouldStart == LOW) {
       drainCoffee();
       drainWater();
       setCoffeePower(true);
       delay(coffeeTurnOnTime);
       setCoffeePower(false);
+      // TODO: send to server that I'm turned off
     }
 
     delay(1000);
